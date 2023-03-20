72636573163f055173434da1f9d3ced424188637
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-03-19 22:19:58 -0300

    did some coding for the readme

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const inquirer = require('inquirer')
 const fs = require('fs')
+const markdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 
@@ -19,18 +20,104 @@ const questions = [
         },
         {
             type: 'input',
-            message: ''
-        }
-    ])
+            message: 'Installation instruction',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'How do you use it?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Who is contributing to this project?',
+            name: 'contribute'
+        },
+        {
+            type: 'input',
+            message: 'How do you test this?',
+            name: 'test'
+        },
+        {
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'githubUserName'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        },
+        {
+            type: 'list',
+            message: 'What license do you use?',
+            choices: ['MIT', 'Apache 2.0'],
+            name: 'license'
+        },
+    ]).then((response) => {
+        const readmeString = `
+${response.license}
+# ${response.title}
+
+## Description
+
+${response.projectDescription}
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Question](#question)
+- [Test](#test)
+
+## Installation
+
+${response.installation}
+
+## Usage
+
+${response.usage}
+
+## Credits
+
+${response.constribute}
+        
+## License
+
+${response.license}
+
+## Question
+
+My github username is [${response.githubUserName}](https://www.github.com/${response.githubUserName}) and you can contact me by email at ${response.email}
+
+        
+## Tests
+
+${response.test}
+    `;
+        fs.writeFile('README.md', readmeString, (err) => {
+            if (err) throw err;
+            console.log('README created!')
+        })
+    })
 ];
 
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    // fs.writeFile('README.MD', readmeString, (err) => {
+    //     if (err) throw err;
+    //     console.log('README created!')
+    // })
+}
 // TODO: Create a function to initialize app
 
-function init() {}
+function init() {
+//     questions
+//     writeToFile ('README.MD', readmeString)
+}
 
 // Function call to initialize app
 init();
